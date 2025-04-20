c2c86eb05ad84d7d505c22a812e6d73274a608dd
jigenesh surampudi <jigeneshsurampudi@gmail.com>
2025-04-20 21:34:14 +0530

    background changes

--- loginform/src/App.js
+++ loginform/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 import LoginPage from './LoginForm/LoginPage';
 
 function App() {
   return (
-    <div >
+    <div className="body">
       <LoginPage />
     </div>
   );
