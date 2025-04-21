b158a69023e0b3ce147f0951213727004ff82114
jigenesh surampudi <jigeneshsurampudi@gmail.com>
2025-04-21 14:52:59 +0530

    signup updated

--- loginform/src/LoginForm/LoginPage.jsx
+++ loginform/src/LoginForm/LoginPage.jsx
@@ -1,27 +1,50 @@
 import React from 'react'
+import { useState } from "react";
 import './LoginPage.css'
 import { FaUser } from "react-icons/fa";
 import { FaLock } from "react-icons/fa";
 const LoginPage = () => {
+  const [action,setAction] =useState("Signup");
+
+  
   return (
-    <section className="mainblock"><form action="">
-    <div className="header"> <h1>Student Login</h1></div>
+    <section className="mainblock">
+      <form action="">
+          
+            <div className="header">
+             
+                <button className={action=="Login" ? "  active": ""}onClick={()=>{setAction("Login")}} >Login</button>
+                <button className={action=="Signup" ? "active": ""}onClick={()=>{setAction("Signup")}}>Signup</button>
+            
+              
+            </div>
+
+                <div className="inputs">
+                  {action=="Login" ? <div></div> :  <div className="inputbox">
+                    <input type="text" placeholder="Name" required/><br></br>
+                  </div>}
+                 
+              
+                  <div className="inputbox">
+                    <input type="text" placeholder="StudentID" required/><FaUser className="icons"/><br></br>
+                  </div>
     
-    <div className="inputbox">
-    <input type="text" placeholder="StudentID" required/><FaUser className="icons"/><br></br>
-    </div>
-    
-    <div className="inputbox">
-    <input type="password" placeholder="password" required/><FaLock className="icons" /><br></br>
-    </div>
-    <div className="remember-forget">
-      <label><input type="checkbox"/>Remember me</label>
-      <a href="#">forget password</a>
-    </div>
-    <div>
-    <button className="loginbtn">Login</button>
-    </div>
-    </form>    
+                  <div className="inputbox">
+                    <input type="password" placeholder="password" required/><FaLock className="icons" /><br></br>
+                  </div>
+                </div>
+               
+                <div className="remember-forget">
+                    <label><input type="checkbox"/>Remember me</label>
+                    <a href="#">forget password</a>
+                </div>
+
+                <div>
+                  <button className="loginbtn">Login</button>
+                </div>
+
+      </form>   
+
     </section>
     
     
